8c060906be1ebf0815d2353f4c7f69fef7d03d9a
Xavier <xseggs@gmail.com>
2022-02-18 16:43:20 -0800

    Autoformatter settings on my end. Shouldn't affect anything, just wanted to get them backed up. Should be able to start writing code soon lol.

--- src/main.py
+++ src/main.py
@@ -1,12 +1,11 @@
 import sys
 
 
-
 def main():
     print('The environment is set up correctly!')
     print('Number of arguments:', len(sys.argv), 'arguments.')
     print('Argument List:', str(sys.argv))
-    
+
+
 if __name__ == '__main__':
     main()
-    
--- src/sanitize_data.py
+++ src/sanitize_data.py
@@ -2,6 +2,7 @@ import sys
 import pandas as pd
 
 
+
 def main():
     print('Data File Path:', sys.argv)
     data = pd.read_csv(str(sys.argv).split()[0])
