7c4c2a4f5816637c7dde571f6e39074667aa7e41
Xavier <xseggs@gmail.com>
2022-03-07 13:58:50 -0800

    quick comment format to make autopep8 happy

--- src/data_analysis.py
+++ src/data_analysis.py
@@ -12,40 +12,44 @@ ie   ex:./.env/python.exe ./src/sanitize_data.py \
         --there's probably a better way to do this for multiple files--
 '''
 
-#Goals for this file: carry out bulk of data analysis
-#Might need to split it up eventually but it should be pretty straightforward
-#once the infrastructure is down
+# Goals for this file: carry out bulk of data analysis
+# Might need to split it up eventually but it should be pretty straightforward
+# once the infrastructure is down
 
-#step 1: regional analysis
-#import data file
-#convert to dataframe
-#group data by state
-#sort by year (not our problem yet; would probably need to be handled outside
-#of this function with some sort of return infrastructure/combining into
-#a new dataframe together) <-second might work better
-#find a source of geodata and apply it to the dataset
-#apply geopandas to the set to plot by location
-#implement later: potentially compare over time and plot largest changes
+#  step 1: regional analysis
+#  import data file
+# convert to dataframe
+# group data by state
+# sort by year (not our problem yet; would probably need to be handled outside
+# of this function with some sort of return infrastructure/combining into
+# a new dataframe together) <-second might work better
+# find a source of geodata and apply it to the dataset
+# apply geopandas to the set to plot by location
+# implement later: potentially compare over time and plot largest changes
+
+
 def regional_analysis(data):
     '''
     Outputs analysis of research institutions by location, given a dataset in
     the form of a dataframe. Creates a geopandas visualization of occurances of
     schools by state.
     '''
-    #group data by state (count all occurances of each)
+    # group data by state (count all occurances of each)
     counts = data.groupby(by='INST_STATE').count()
-    #print(counts.head()) #for testing
-    #apply geodata:
-    #shapefile from
-    #https://hub.arcgis.com/datasets/CMHS::states-shapefile/explore?location=28.177808%2C-105.995557%2C3.79
-    #intent is to test this file once I have geopandas up and running
+    # print(counts.head()) #for testing
+    # apply geodata:
+    # shapefile from
+    # https://hub.arcgis.com/datasets/CMHS::states-shapefile/explore?location=28.177808%2C-105.995557%2C3.79
+    # intent is to test this file once I have geopandas up and running
 
-#step 2: research subject focus
-#group by research areas
-#sum square footage of areas
-#Plot or print list of areas with most institutions, and list of areas
-#with most square footage in institutions
-#do max() and min() of subjects and spit those out (print?)
+# step 2: research subject focus
+# group by research areas
+# sum square footage of areas
+# Plot or print list of areas with most institutions, and list of areas
+# with most square footage in institutions
+# do max() and min() of subjects and spit those out (print?)
+
+
 def subject_focus(data):
     '''
     Outputs informatin on research subjects and the broadness of their
@@ -71,20 +75,20 @@ def subject_focus(data):
 
     # sorts compiled data from largest to smallest; easy to reorder if needed
     subj_counts = dict(sorted(subj_counts.items(),
-                       key = lambda item: item[1], reverse=True))
+                       key=lambda item: item[1], reverse=True))
     subj_nasf = dict(sorted(subj_nasf.items(),
-                     key = lambda item: item[1], reverse=True))
+                     key=lambda item: item[1], reverse=True))
     print(subj_counts)
     print(subj_nasf)
     # still need to plot all this!
 
 
-#step 3: monetary support of instituions
-#sum amounts of R/R and new construction per institution
-#pair these sums with the listed sources of funding
-#compare the sums of funding sources and occurances of funding sources (as in 2)
-#stretch: look at correlation between institution type and funding source
-#or state and funding source
+# step 3: monetary support of instituions
+# sum amounts of R/R and new construction per institution
+# pair these sums with the listed sources of funding
+# compare the sums of funding sources and occurances of funding sources (as in 2)
+# stretch: look at correlation between institution type and funding source
+# or state and funding source
 def amount_of_growth(data):
     '''
     Outputs analysis of growth of research institutions, represented by the
@@ -95,15 +99,16 @@ def amount_of_growth(data):
 
 
 def main():
-    #my path, dropping in so I can call from atom
+    # my path, dropping in so I can call from atom
     path = './data/2019_sanitized.csv'
 
-    #import data file, convert to dataframe- idk if this works
+    # import data file, convert to dataframe- idk if this works
     dpath = sys.argv[1:]
 
-    data = pd.read_csv(abspath(path), encoding='ISO-8859-1') #might work
+    data = pd.read_csv(abspath(path), encoding='ISO-8859-1')  # might work
     regional_analysis(data)
     subject_focus(data)
+
 
 if __name__ == '__main__':
     main()
