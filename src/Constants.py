16244e130ee98fa678ecf4d56bc20968d5da3247
Xavier <xseggs@gmail.com>
2022-03-10 16:05:56 -0800

    multi-year data sanitization, just need to manually assemble lists for prior to 2015

--- src/Constants.py
+++ src/Constants.py
@@ -3,6 +3,14 @@
 NASF_COLUMNS = {2019: [  # The collumn names per year that have to do with NASF
     'NASF_AG', 'NASF_BIO', 'NASF_COS', 'NASF_ENG', 'NASF_GEO', 'NASF_HLTH',
     'NASF_MATH', 'NASF_NR', 'NASF_PHY', 'NASF_PSY', 'NASF_SOC', 'NASF_OTH',
+    # The collumn names per year that have to do with NASF
+    'NASF_CLIN_TRIAL', 'NASF_MED'], 2017: [
+    'NASF_AG', 'NASF_BIO', 'NASF_COS', 'NASF_ENG', 'NASF_GEO', 'NASF_HLTH',
+    'NASF_MATH', 'NASF_NR', 'NASF_PHY', 'NASF_PSY', 'NASF_SOC', 'NASF_OTH',
+    # The collumn names per year that have to do with NASF
+    'NASF_CLIN_TRIAL', 'NASF_MED'], 2015: [
+    'NASF_AG', 'NASF_BIO', 'NASF_COS', 'NASF_ENG', 'NASF_GEO', 'NASF_HLTH',
+    'NASF_MATH', 'NASF_NR', 'NASF_PHY', 'NASF_PSY', 'NASF_SOC', 'NASF_OTH',
     'NASF_CLIN_TRIAL', 'NASF_MED']}
 
 # General but neccessary collumns.
@@ -11,9 +19,9 @@ GENERAL_COLUMNS = {2019: [
     'YEAR', 'INST_ID', 'INST_NAME', 'SUBMISSION_FLAG',
     'INST_TYPE', 'INST_STATE', 'TOC_CODE', 'EXP_TOT_2018'], 2017: [
     'YEAR', 'INST_ID', 'INST_NAME', 'SUBMISSION_FLAG',
-    'INST_TYPE', 'INST_STATE', 'TOC_CODE', 'EXP_TOT_2018'], 2015: [
+    'INST_TYPE', 'INST_STATE', 'TOC_CODE', 'EXP_TOT_2016'], 2015: [
     'YEAR', 'INST_ID', 'INST_NAME', 'SUBMISSION_FLAG',
-    'INST_TYPE', 'INST_STATE', 'TOC_CODE', 'EXP_TOT_2018']}
+    'INST_TYPE', 'INST_STATE', 'TOC_CODE', 'EXP_TOT_2014']}
 
 # This was originally going to be for the insertion of NaN into the dataset
 # per row, but that ended up not being neccessary. Currently the only used
@@ -44,5 +52,45 @@ FILTER_VALUES = {
      ['1', 'PRR_AG', 'PRR_BIO', 'PRR_COS', 'PRR_ENG', 'PRR_GEO',
       'PRR_HLTH', 'PRR_MATH', 'PRR_NR', 'PRR_PHY', 'PRR_PSY', 'PRR_SOC',
       'PRR_OTH']
-     }
+     }, 2017:
+    {'SUBMISSION_FLAG': ['Y', None],
+     # 'RRBOX' onward is not needed, I'm just
+     # keeping it in here in case it comes in useful later.
+     'RRBOX':
+     ['1', 'RR_AG', 'RR_BIO', 'RR_COS', 'RR_ENG', 'RR_GEO', 'RR_HLTH',
+      'RR_MATH', 'RR_NR', 'RR_PHY', 'RR_PSY', 'RR_SOC', 'RR_OTH',
+      'RR_TFUND', 'RR_CLIN_TRIAL', 'RR_MED'],
+     'NCBOX': ['1', 'NC_FED', 'NC_STA', 'NC_INST', 'NC_TFUND'],
+     'PNCBOX':
+     ['1', 'PNC_AG_1', 'PNC_BIO_1', 'PNC_COS_1', 'PNC_ENG_1', 'PNC_GEO_1',
+        'PNC_HLTH_1', 'PNC_MATH_1', 'PNC_NR_1', 'PNC_PHY_1', 'PNC_PSY_1',
+        'PNC_SOC_1', 'PNC_OTH_1'
+        'PNC_AG_2', 'PNC_BIO_2', 'PNC_COS_2', 'PNC_ENG_2', 'PNC_GEO_2',
+        'PNC_HLTH_2', 'PNC_MATH_2', 'PNC_NR_2', 'PNC_PHY_2', 'PNC_PSY_2',
+        'PNC_SOC_2', 'PNC_OTH_2'],
+     'PRRBOX':
+     ['1', 'PRR_AG', 'PRR_BIO', 'PRR_COS', 'PRR_ENG', 'PRR_GEO',
+      'PRR_HLTH', 'PRR_MATH', 'PRR_NR', 'PRR_PHY', 'PRR_PSY', 'PRR_SOC',
+      'PRR_OTH']
+     }, 2015:
+    {'SUBMISSION_FLAG': ['Y', None],
+     # 'RRBOX' onward is not needed, I'm just
+     # keeping it in here in case it comes in useful later.
+     'RRBOX':
+     ['1', 'RR_AG', 'RR_BIO', 'RR_COS', 'RR_ENG', 'RR_GEO', 'RR_HLTH',
+      'RR_MATH', 'RR_NR', 'RR_PHY', 'RR_PSY', 'RR_SOC', 'RR_OTH',
+      'RR_TFUND', 'RR_CLIN_TRIAL', 'RR_MED'],
+     'NCBOX': ['1', 'NC_FED', 'NC_STA', 'NC_INST', 'NC_TFUND'],
+     'PNCBOX':
+     ['1', 'PNC_AG_1', 'PNC_BIO_1', 'PNC_COS_1', 'PNC_ENG_1', 'PNC_GEO_1',
+        'PNC_HLTH_1', 'PNC_MATH_1', 'PNC_NR_1', 'PNC_PHY_1', 'PNC_PSY_1',
+        'PNC_SOC_1', 'PNC_OTH_1'
+        'PNC_AG_2', 'PNC_BIO_2', 'PNC_COS_2', 'PNC_ENG_2', 'PNC_GEO_2',
+        'PNC_HLTH_2', 'PNC_MATH_2', 'PNC_NR_2', 'PNC_PHY_2', 'PNC_PSY_2',
+        'PNC_SOC_2', 'PNC_OTH_2'],
+     'PRRBOX':
+     ['1', 'PRR_AG', 'PRR_BIO', 'PRR_COS', 'PRR_ENG', 'PRR_GEO',
+      'PRR_HLTH', 'PRR_MATH', 'PRR_NR', 'PRR_PHY', 'PRR_PSY', 'PRR_SOC',
+      'PRR_OTH']
+     },
 }
--- src/sanitize_data.py
+++ src/sanitize_data.py
@@ -8,10 +8,8 @@ Dataset information can be found in
 '''
 from pathlib import Path
 import sys
-from os.path import abspath
 import pandas as pd
 from Constants import NASF_COLUMNS, GENERAL_COLUMNS, FILTER_VALUES
-# import numpy as np
 
 
 ''' call using this syntax in terminal:
@@ -23,6 +21,32 @@ from Constants import NASF_COLUMNS, GENERAL_COLUMNS, FILTER_VALUES
 '''
 
 
+def _parse_years(years):
+    if isinstance(years, list):
+        years = [int(year) for year in years]
+        return years.sort()
+    elif isinstance(years, str):
+        return list(
+            range(
+                int(years.split(':')[0]),
+                int(years.split(':')[-1]) + 1, 2))
+    elif isinstance(years, int):
+        return list(years)
+    else:
+        return 'Invalid YEAR value! Program will crash shortly.'
+
+
+def _deparse_years(years_list):
+    if isinstance(years_list, list):
+        if len(years_list) > 1:
+            years = str(min(years_list)) + '-' + str(max(years_list))
+        else:
+            years = str(years_list[0])
+        return years
+    else:
+        return 'Invalid YEAR values! Program will crash shortly.'
+
+
 def main():
     '''
     Handles main chunk of data sanitization, including initialization and
@@ -32,37 +56,48 @@ def main():
     from Constants.py
     '''
     # Basic argument handling and reporting back to user for debug purposes.
-    origin_path, location, year = sys.argv[1:]
-    origin_path = abspath(origin_path)
-    location = abspath(location)
-    year = int(year)
-    print('Data File Path:', origin_path)
-    print('Location Path: ', location)
-    print('Year: ', year)
-
-    # Reads data from CSV, and then retrieves and aggregates into single list.
-    data = pd.read_csv(origin_path, encoding='ISO-8859-1')
-    valid_columns = [GENERAL_COLUMNS[year], NASF_COLUMNS[year]]
-    valid_columns.extend(FILTER_VALUES[year].keys())
-    # Iterates through keys of FILTER_VALUES and adds all values to
-    # list of valid_columns.
-    valid_columns.extend(map(lambda x: FILTER_VALUES[year][x][1:],
+    origin_path, location, years_str = sys.argv[1:]
+    # origin_path = abspath(origin_path)
+    # location = abspath(location)
+    print('Data File Directory:', origin_path)
+    print('Location Directory: ', location)
+    years = _parse_years(years_str)
+    years_str = _deparse_years(years)
+    print('Year(s): ' + years_str)
+    data = dict()
+    for year in years:
+        # Reads data from CSV,
+        # and then retrieves and aggregates into single list.
+        data[year] = pd.read_csv(
+            str(origin_path + 'facilities_' + str(year) + '.csv'),
+            encoding='ISO-8859-1')
+        valid_columns = [GENERAL_COLUMNS[year], NASF_COLUMNS[year]]
+        valid_columns.extend(FILTER_VALUES[year].keys())
+        # Iterates through keys of
+        # FILTER_VALUES and adds all values to
+        # list of valid_columns.
+        valid_columns.extend(map(lambda x: FILTER_VALUES[year][x][1:],
                              FILTER_VALUES[year].keys()))
-    # Since FILTER_VALUES is a dictionary of list-of-lists with years as keys,
-    # once the values (list-of-lists) have been added they must then be
-    # unpacked to create a single list of Strings. More info in Constants.py.
-    valid_columns = [item for sublist in valid_columns for item in sublist]
-    # Now that a list of all columns we want to keep has been assembled,
-    # we can finally cut the dataset down to only those columns.
-    data = data.filter(items=valid_columns)
-    # Filters only for values which have responses. Frankly I'm not sure
-    # why they'd include non-Y answers in the dataset.
-    data = data[data['SUBMISSION_FLAG'] == 'Y'].drop(
-        labels='SUBMISSION_FLAG', axis=1)
-    data = data.replace('S', 0)
-    Path(location).parent.mkdir(parents=True, exist_ok=True)
-    data.to_csv(location)
-    print('Export Successful!')
+        # Since FILTER_VALUES is a dictionary of
+        # list-of-lists with years as keys, once the
+        # values (list-of-lists) have been added they must then be
+        # unpacked to create a single list of Strings.
+        # More info in Constants.py.
+        valid_columns = [item for sublist in valid_columns for item in sublist]
+        # Now that a list of all columns we want to keep has been assembled,
+        # we can finally cut the dataset down to only those columns.
+        data[year] = data[year].filter(items=valid_columns)
+        # Filters only for values which have responses. Frankly I'm not sure
+        # why they'd include non-Y answers in the dataset.
+        data[year] = data[year][data[year]['SUBMISSION_FLAG'] == 'Y'].drop(
+            labels='SUBMISSION_FLAG', axis=1)
+        data[year] = data[year].replace('S', 0)
+    Path(str(location + years_str + 'sanitized.csv')
+         ).parent.mkdir(parents=True, exist_ok=True)
+    data = pd.concat(data.values())
+    data.to_csv(str(location + years_str + '_sanitized.csv'))
+    print('Export Successful! Path: ', Path(
+        str(location + years_str + 'sanitized.csv')))
 
 
 if __name__ == '__main__':
